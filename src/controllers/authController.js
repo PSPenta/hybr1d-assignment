1d26fc6d0f58ca45ef88d2d4fcb468c17cebb054
Pritesh Shinde <pritesh.s@purplle.com>
2022-08-01 20:22:15 +0530

    Auth routes and functionality created

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,23 +1,26 @@
-/* eslint-disable no-undef */
-const { compare } = require('bcrypt');
+const { compare, hash } = require('bcrypt');
 const { StatusCodes } = require('http-status-codes');
 const { sign } = require('jsonwebtoken');
 
+const { model } = require('../config/dbConfig');
 const { jwt } = require('../config/serverConfig');
-const { response } = require('../helpers/utils');
+const { response, checkIfDataExists } = require('../helpers/utils');
 
 exports.jwtLogin = async (req, res) => {
   try {
-    // const userData = await model('User').findOne({ username: req.body.username });
-    // const userData = await model('User').findAll({ where: { username: req.body.username } });
+    const userData = await model('User').findAll({ where: { username: req.body.username } });
     // Note: If using Sequelize, update userData to userData[0] for all the following occurrences
     let token = '';
-    if (userData && await compare(req.body.password, userData.password)) {
+    if (
+      checkIfDataExists(userData)
+      && userData[0]
+      && await compare(req.body.password, userData[0].password)
+    ) {
       token = sign(
         {
-          username: userData.username,
-          // eslint-disable-next-line no-underscore-dangle
-          userId: userData._id.toString()
+          username: userData[0].username,
+          userId: userData[0].id.toString(),
+          role: userData[0].role
         },
         jwt.secret,
         { expiresIn: jwt.expireIn }
@@ -29,7 +32,7 @@ exports.jwtLogin = async (req, res) => {
     return res.status(StatusCodes.BAD_REQUEST).json(response('User not found!'));
   } catch (error) {
     console.error(error);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response('Something went wrong!'));
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response('Internal server error!'));
   }
 };
 
@@ -48,6 +51,29 @@ exports.jwtLogout = async (req, res) => {
     return res.status(StatusCodes.UNAUTHORIZED).json(response('You are not authorized to access this page!'));
   } catch (error) {
     console.error(error);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response('Something went wrong!'));
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response('Internal server error!'));
+  }
+};
+
+exports.register = async (req, res) => {
+  try {
+    const data = await model('User').findAll({ where: { username: req.body.username } });
+    if (!data || !data[0]) {
+      const hashedPassword = await hash(req.body.password, 256);
+      const user = await model('User').create({
+        username: req.body.username,
+        password: hashedPassword,
+        role: req.body.role.toLowerCase()
+      });
+
+      if (user) {
+        return res.status(StatusCodes.CREATED).json(response(null, true, { message: 'User added successfully!' }));
+      }
+      return res.status(StatusCodes.BAD_REQUEST).json(response('Something went wrong!'));
+    }
+    return res.status(StatusCodes.BAD_REQUEST).json(response('Username is already taken!'));
+  } catch (error) {
+    console.error(error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response('Internal server error!'));
   }
 };
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,28 +1,32 @@
 const { StatusCodes } = require('http-status-codes');
 const { verify } = require('jsonwebtoken');
 
+const { model } = require('../config/dbConfig');
 const { jwt } = require('../config/serverConfig');
 const { response } = require('../helpers/utils');
 
 // eslint-disable-next-line consistent-return
-exports.jwtAuth = (req, res, next) => {
+exports.jwtAuth = async (req, res, next) => {
   try {
     if (req.headers.authorization) {
       const token = req.headers.authorization.split(' ')[1];
-      // const blacklistedToken = await model('Blacklist').findOne({ token: token });
-      // const blacklistedToken = await model('Blacklist').findAll({ where: { token: token } });
-      // eslint-disable-next-line no-undef
-      if (!blacklistedToken) {
-        const decodedToken = verify(token, jwt.secret, (err, decoded) => {
-          if (err) {
-            console.error('JWT Error:', err);
-            return res.status(StatusCodes.UNAUTHORIZED).json(response('Your login session is either expired or the token is invalid, please try logging in again!'));
+      if (token) {
+        const blacklistedToken = await model('Blacklist').findAll({ where: { token } });
+        if (!blacklistedToken) {
+          const decodedToken = verify(token, jwt.secret, (err, decoded) => {
+            if (err) {
+              console.error('JWT Error:', err);
+              return res.status(StatusCodes.UNAUTHORIZED).json(response('Your login session is either expired or the token is invalid, please try logging in again!'));
+            }
+            return decoded;
+          });
+          if (decodedToken) {
+            req.userId = decodedToken.userId;
+            req.userType = decodedToken.role;
+            next();
+          } else {
+            return res.status(StatusCodes.UNAUTHORIZED).json(response('You are not authorized to access this page!'));
           }
-          return decoded;
-        });
-        if (decodedToken) {
-          req.userId = decodedToken.userId;
-          next();
         } else {
           return res.status(StatusCodes.UNAUTHORIZED).json(response('You are not authorized to access this page!'));
         }
