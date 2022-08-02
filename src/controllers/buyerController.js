d061a716da9c9d720357760366aa3c7bf4fe22ec
Pritesh Shinde <pritesh.s@purplle.com>
2022-08-02 23:44:39 +0530

    Seller catalog API created

--- src/controllers/buyerController.js
+++ src/controllers/buyerController.js
@@ -15,3 +15,27 @@ exports.listOfSellers = async (req, res) => {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response('Internal server error!'));
   }
 };
+
+exports.sellerCatalog = async (req, res) => {
+  try {
+    if (checkIfDataExists(req.params.sellerId)) {
+      const seller = await model('User').findOne({ where: { id: req.params.sellerId } });
+      if (checkIfDataExists(seller)) {
+        const catalog = await model('Catalog').findOne({ where: { userId: req.params.sellerId } });
+        if (checkIfDataExists(catalog)) {
+          const products = await model('Product').findAll({ where: { catalogId: catalog.id }, attributes: ['id', 'name', 'price'] });
+          if (checkIfDataExists(products)) {
+            return res.json(response(null, true, { products }));
+          }
+          return res.status(StatusCodes.BAD_REQUEST).json(response('No products found!'));
+        }
+        return res.status(StatusCodes.BAD_REQUEST).json(response('No catalog found for this seller!'));
+      }
+      return res.status(StatusCodes.BAD_REQUEST).json(response('Invalid seller!'));
+    }
+    return res.status(StatusCodes.BAD_REQUEST).json(response('Something went wrong!'));
+  } catch (error) {
+    console.error(error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response('Internal server error!'));
+  }
+};
--- src/routes/buyerRoutes.js
+++ src/routes/buyerRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { param } = require('express-validator');
 
 const dependencies = require('./routesDependencies');
 
@@ -30,6 +31,48 @@ const dependencies = require('./routesDependencies');
 router.get(
   '/list-of-sellers',
   dependencies.controllers.buyer.listOfSellers
+);
+
+/**
+ * @swagger
+ * /buyer/seller-catalog/:sellerId:
+ *  get:
+ *    tags:
+ *      - Buyer
+ *    name: Seller Catalog API
+ *    summary: This api provides the catalog info of specified seller.
+ *    consumes:
+ *      - application/json
+ *    produces:
+ *      - application/json
+ *    parameters:
+ *      - name: Param Data
+ *        in: param
+ *        schema:
+ *         type: object
+ *         properties:
+ *          sellerId:
+ *            type: number
+ *        required:
+ *         - sellerId
+ *    responses:
+ *      200:
+ *        description: All the blogs and videos.
+ *      422:
+ *        description: Input validation error messages.
+ *      500:
+ *        description: Internal server error.
+ */
+router.get(
+  '/seller-catalog/:sellerId',
+  [
+    param('sellerId')
+      .exists().withMessage('Seller is mandatory!')
+      .isInt({ min: 0, max: 99999999999 })
+      .withMessage('Invalid seller!')
+  ],
+  dependencies.middlewares.requestValidator.validateRequest,
+  dependencies.controllers.buyer.sellerCatalog
 );
 
 module.exports = router;
