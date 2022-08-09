7c8ac2b1c1ebe3c6f8eaf7255a817e085f7c0769
Pritesh Shinde <pritesh.s@purplle.com>
2022-08-09 21:23:12 +0530

    JWT auth token expiry check issue resolved

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -22,20 +22,21 @@ exports.jwtAuth = async (req, res, next) => {
       return res.status(StatusCodes.UNAUTHORIZED).json(response('You are not authorized to access this page!'));
     }
 
-    const decodedToken = verify(token, jwt.secret, (err, decoded) => {
+    // eslint-disable-next-line consistent-return
+    verify(token, jwt.secret, (err, decoded) => {
       if (err) {
         console.error('JWT Error:', err);
         return res.status(StatusCodes.UNAUTHORIZED).json(response('Your login session is either expired or the token is invalid, please try logging in again!'));
       }
-      return decoded;
+
+      if (decoded && decoded.userId && decoded.role) {
+        req.userId = decoded.userId;
+        req.userType = decoded.role;
+        next();
+      } else {
+        return res.status(StatusCodes.UNAUTHORIZED).json(response('You are not authorized to access this page!'));
+      }
     });
-    if (decodedToken) {
-      req.userId = decodedToken.userId;
-      req.userType = decodedToken.role;
-      next();
-    } else {
-      return res.status(StatusCodes.UNAUTHORIZED).json(response('You are not authorized to access this page!'));
-    }
   } catch (error) {
     console.error(error);
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(response('Internal server error!'));
